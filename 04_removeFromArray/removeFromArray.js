a67d38309b404b9262ccd6aae0a5a264a77e371d
Colton Randall <coltonrandall.nz@gmail.com>
2023-09-15 15:01:07 +1200

    Solve remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+	/**
+	 * TODO
+	 * this is a fair simpler option:
+	 * return array.filter((index) => !args.includes(index));
+	 */
 
+	// create new array
+	const newArray = [];
+
+	// loop through each item and add to new array
+	array.forEach((item) => {
+		// chuck each item into the new array unless it's the item
+		if (!args.includes(item)) {
+			newArray.push(item);
+		}
+	});
+	return newArray;
 };
 
 // Do not edit below this line
