3a63b5d869a89e9b5e1678aea6946788a53efbbc
Colton Randall <coltonrandall.nz@gmail.com>
2023-09-19 09:12:24 +1200

    Solve exercise 07 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (numberInFahrenheit) {
+	/* 
+    formula: F -> C 
+    xC = (xF - 32) x 5/9
+  */
+
+	let result = (numberInFahrenheit - 32) * (5 / 9);
+
+	return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (numberInCelsius) {
+	/* 
+    formula: C -> F 
+    xF = (xC x 9/5 32) + 32
+  */
+
+	let result = numberInCelsius * (9 / 5) + 32;
+
+	return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
